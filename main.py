7c89d1be9a607925e6a327200ed0a550f98c627f
Justis Nazirbage <jnazirbage@gmail.com>
2023-10-07 19:34:37 -0400

    added import generate_events

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+# office_hours = Event("You do not understand this chapter\’s material. Luckily, your professor has office hours today. But you do not like your professor. He has a hard to understand accent and every conversation with him is just an endless drain. At the same time, this may be the only way to properly wrap your head around the material. Is it worth going? ")
+# office_hours.yes(0,1,0,-2)
+# office_hours.no(0,-1,0,0)
+
+# # testing
+# boysie = Player()
+# condition = int(input(office_hours.blurb))
+# boysie.event_effects(office_hours, condition)
+# boysie.event_effects(office_hours, condition)
+# print(boysie.coding, boysie.education, boysie.social, boysie.mental)
+
 from event import Event
 from player import Player
+from create_events import generate_events
 import random
 
 def main():
