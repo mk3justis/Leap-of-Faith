fa3379584d4238e2db332494915095ef13820efe
MJMrigg <130166245+MJMrigg@users.noreply.github.com>
2023-10-07 18:14:22 -0400

    Fixed the issue with the creation

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ print(boysie.coding, boysie.education, boysie.social, boysie.mental)
 def main():
     name = input("What is your name?")
     dream_job = input("What is your dream company? ")
-    plauer = Player(0, 0, 0, 0)
+    plauer = Player(0, 0, 0, 0, 0)
     focus_number = 1
     focus_attribute = 4
     while focus_number <= 3:
@@ -31,63 +31,64 @@ def main():
             print("3 for Education")
         if plauer.mental == 0:
             print("4 for Mental")
-        focus = input("")
+        focus = int(input(""))
 
-        if (focus == 1 and plauer.coding) == 0:
-            plauer.coding += focus_attribute
+        if (focus == 1 and plauer.coding == 0):
+            plauer.coding = plauer.coding + focus_attribute
             print(plauer.coding)
-        elif focus == 2 and plauer.social == 0:
+        elif (focus == 2 and plauer.social == 0):
             plauer.social += focus_attribute
-        elif focus == 3 and plauer.education:
+            print(plauer.social)
+        elif (focus == 3 and plauer.education == 0):
             plauer.education += focus_attribute
-        elif focus == 4 and plauer.mental:
+            print(plauer.education)
+        elif (focus == 4 and plauer.mental == 0):
             plauer.mental += focus_attribute
+            print(plauer.mental)
         focus_attribute -= 1
         focus_number += 1
 
-        def generate_events():
-            week = 1
-            while (week < 32):
-                event_type = 0  # Generate a random number that will determine the event type
-                event_number = 0  # Event type. 1 = Rare, 2-8 = common, 9-10 = none
-                event = 0
-                if (week == 8):
-                    pass  # some code for exam 1 goes here
-                elif (week == 16):
-                    pass  # some code for the career fair goes here
-                elif (week == 24):
-                    pass  # some code for exam 2 goes here
-                else:
-                    event_number = random.randint(1, 10)
-                # Determine the event type:
-                if (event_number == 1):  # Rare
-                    event_type = 1
-                elif (event_number > 1 and event_number < 9):  # Common
-                    event_type = 2
-                elif (event_number >= 9):  # None
-                    event_type = 3
+        week = 1
+        while (week < 32):
+            event_type = 0  # Generate a random number that will determine the event type
+            event_number = 0  # Event type. 1 = Rare, 2-8 = common, 9-10 = none
+            event = 0
+            if (week == 8):
+                pass  # some code for exam 1 goes here
+            elif (week == 16):
+                pass  # some code for the career fair goes here
+            elif (week == 24):
+                pass  # some code for exam 2 goes here
+            else:
+                event_number = random.randint(1, 10)
+            # Determine the event type:
+            if (event_number == 1):  # Rare
+                event_type = 1
+            elif (event_number > 1 and event_number < 9):  # Common
+                event_type = 2
+            elif (event_number >= 9):  # None
+                event_type = 3
 
-                # Generate the event:
-                if (event_number == 1):
-                    event = random.randint(29, 35)
-                elif (event_number == 2):
-                    event = random.randint(1, 23)
+            # Generate the event:
+            if (event_number == 1):
+                event = random.randint(29, 35)
+            elif (event_number == 2):
+                event = random.randint(1, 23)
 
-                # This section of code prevents an event from happening twice
-                # It uses an array to store the number associated with that event
-                # When that event happens, the number becomes 0, ensuring that it will never happen again
-                events = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-                          27, 28, 29, 30, 31, 32, 33, 34, 35]
-                event_that_happened = Event("This is an example")
-                for possibility in events:
-                    if (event == possibility):
-                        check_event(possibility, event_that_happened)
-                        possibility = 0
-                        break
+            # This section of code prevents an event from happening twice
+            # It uses an array to store the number associated with that event
+            # When that event happens, the number becomes 0, ensuring that it will never happen again
+            events = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+                      27, 28, 29, 30, 31, 32, 33, 34, 35]
+            event_that_happened = Event("This is an example")
+            for possibility in events:
+                if (event == possibility):
+                    check_event(possibility, event_that_happened)
+                    possibility = 0
+                    break
 
-                if (event_number == 3):
-                    pass
-                week = week + 1
-    generate_events()
+            if (event_number == 3):
+                pass
+            week = week + 1
 
 main()
