0f37f99526f0b291d38fd78dd645c8d843d330c9
MJMrigg <130166245+MJMrigg@users.noreply.github.com>
2023-10-07 16:54:57 -0400

    Added A bunch of Stuff

---
+++ player.py
@@ -0,0 +1,8 @@
+# Create a player class with the necessary attributes
+class Player:
+    def __init__(self, _coding, _education, _social, _mental):
+        self.coding = _coding
+        self.education = _education
+        self.social = _social
+        self.mental = _mental
+        self.energy = 0
