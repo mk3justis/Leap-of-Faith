efc17060950769f744c6c067c08c0ec18951a212
Justis Nazirbage <103616292+mk3justis@users.noreply.github.com>
2023-10-08 18:13:24 -0400

    Added doc string to main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-# office_hours = Event("You do not understand this chapter\’s material. Luckily, your professor has office hours today. But you do not like your professor. He has a hard to understand accent and every conversation with him is just an endless drain. At the same time, this may be the only way to properly wrap your head around the material. Is it worth going? ")
-# office_hours.yes(0,1,0,-2)
-# office_hours.no(0,-1,0,0)
+'''
+This is a text-based game that takes the user through a story of one year at university.
+It is full of random events, goals, and emotions.
+Enjoy!
 
-# # testing
-# boysie = Player()
-# condition = int(input(office_hours.blurb))
-# boysie.event_effects(office_hours, condition)
-# boysie.event_effects(office_hours, condition)
-# print(boysie.coding, boysie.education, boysie.social, boysie.mental)
+Developed by Justis Nazirbage and Matthew Grigg for the FPU Fall '23 Respawn Game Jam event.
+'''
+
 from player import Player
 from create_events import generate_events
 from exams import *
